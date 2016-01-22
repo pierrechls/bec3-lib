e5fa5c16af90f9f3aace76b10ec2d0cce3b762b4
Pierre Charles <pierre.chls@gmail.com>
2016-01-22 14:28:00 +0100

    Delete SDL information in main

--- project/template/main.cpp
+++ project/template/main.cpp
@@ -24,39 +24,5 @@ int main(int argc, const char **argv) {
 	cout << "TestLight: " << mySession.getObjectState( "TestLight" ).getBool() << endl;
 	cout << "MSG: "       << mySession.getObjectState( "MSG" ).getString()     << endl;
 
-    // Initialize SDL and open a window
-    GLuint screenWidth = 800, screenHeight = 600;
-    SDLWindowManager windowManager(screenWidth, screenHeight, "Bec3 Artistic");
-
-    // Initialize glew for OpenGL3+ support
-    glewExperimental = GL_TRUE;
-    GLenum glewInitError = glewInit();
-    if(GLEW_OK != glewInitError) {
-        std::cerr << glewGetErrorString(glewInitError) << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    //Define the viewport dimensions
-    glViewport(0, 0, screenWidth, screenHeight);
-
-    // Setup some OpenGL options
-    glEnable(GL_DEPTH_TEST);
-
-    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl << std::endl;
-
-
-    Engine Artistic;
-
-
-    //Application loop:
-    bool done = false;
-
-    while(!done) {
-        Artistic.run(&windowManager, screenWidth, screenHeight, &done);
-    }
-
-    Artistic.stop();
-
     return 0;
 }
